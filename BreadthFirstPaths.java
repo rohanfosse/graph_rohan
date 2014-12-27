186a37a07277c038693e5ccf836db0048705b5b8
Rohan <rohan.fosse@gmail.com>
2014-12-27 18:06:00 +0100

    Initial commit

---
+++ BreadthFirstPaths.java
@@ -0,0 +1,78 @@
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Stack;
+
+
+public class BreadthFirstPaths {
+    private static final int INFINITY = Integer.MAX_VALUE;
+    private boolean[] marked;
+    private int[] edgeTo;
+    private int[] distTo;
+
+    /**
+     * Calcule le plus court chemin entre le sommet d'origine <tt>s</tt>
+     * aet les autres sommet du graphe  <tt>G</tt>.
+     *
+     * @param G le graphe
+     * @param s le sommet d'origine
+     */
+    public BreadthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        distTo = new int[G.V()];
+        edgeTo = new int[G.V()];
+        bfs(G, s);
+    }
+
+
+    private void bfs(Graph G, int s) {
+        Queue<Integer> q = new Queue<Integer>();
+        for (int v = 0; v < G.V(); v++) distTo[v] = INFINITY;
+        distTo[s] = 0;
+        marked[s] = true;
+        q.enqueue(s);
+
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    q.enqueue(w);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * @param v le sommet
+     * @return <tt>true</tt> isi il y a un chemin vers le sommet v et <tt>false </tt> sinon
+     */
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    /**
+     * @param v le sommet
+     * @return tle nombre de sommet du plus court chemi vers <t>v</t>
+     */
+    public int distTo(int v) {
+        return distTo[v];
+    }
+
+    /**
+     * @param v le sommet
+     * @return la liste des sommets du plus court chemin comme un Iterable
+     */
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<Integer>();
+        int x;
+        for (x = v; distTo[x] != 0; x = edgeTo[x])
+            path.push(x);
+        path.push(x);
+        return path;
+    }
+}
---
+++ main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class main {
+
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        String Methode = args[0];
+        String in = new String(args[1]);
+        Graph G = new Graph(in);
+       // System.out.println(G);
+        if (Methode.equals("e")) System.out.println(G.diametre());
+        else if (Methode.equals("h")) System.out.println(G.Habib());
+
+
+    }
+}
