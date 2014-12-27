21d72ebbc7410d554271e0cc5da18d77f9f04457
Rohan <rohan.fosse@gmail.com>
2014-12-27 20:25:30 +0100

    ajout de majuscule

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-public class main {
+public class Main {
 
 
     public static void main(String[] args) throws IOException, URISyntaxException {
